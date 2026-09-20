ea56a8238ba50d90bbdb29dec602a3ae3840f269
Burmalgram <burmalgram@local>
2026-09-20 21:59:34 +0200

    Fix PostboxUpdateMessage return type using StoreMessage instead of Message

--- submodules/TelegramCore/Sources/State/AccountStateManagementUtils.swift
+++ submodules/TelegramCore/Sources/State/AccountStateManagementUtils.swift
@@ -4470,7 +4470,7 @@ func replayFinalState(
                                     if !attributes.contains(where: { $0 is DeletedMessageAttribute }) {
                                         attributes.append(DeletedMessageAttribute(deletedAt: Int32(Date().timeIntervalSince1970)))
                                     }
-                                    return .update(currentMessage.withUpdatedAttributes(attributes))
+                                    return .update(StoreMessage(id: currentMessage.id, customStableId: nil, globallyUniqueId: currentMessage.globallyUniqueId, groupingKey: currentMessage.groupingKey, threadId: currentMessage.threadId, timestamp: currentMessage.timestamp, flags: StoreMessageFlags(currentMessage.flags), tags: currentMessage.tags, globalTags: currentMessage.globalTags, localTags: currentMessage.localTags, forwardInfo: currentMessage.forwardInfo.flatMap(StoreMessageForwardInfo.init), authorId: currentMessage.author?.id, text: currentMessage.text, attributes: attributes, media: currentMessage.media))
                                 })
                             }
                         }
--- submodules/TelegramCore/Sources/TelegramEngine/Messages/DeleteMessages.swift
+++ submodules/TelegramCore/Sources/TelegramEngine/Messages/DeleteMessages.swift
@@ -76,7 +76,7 @@ public func _internal_deleteMessages(transaction: Transaction, mediaBox: MediaBo
                     if !attributes.contains(where: { $0 is DeletedMessageAttribute }) {
                         attributes.append(DeletedMessageAttribute(deletedAt: Int32(Date().timeIntervalSince1970)))
                     }
-                    return .update(currentMessage.withUpdatedAttributes(attributes))
+                    return .update(StoreMessage(id: currentMessage.id, customStableId: nil, globallyUniqueId: currentMessage.globallyUniqueId, groupingKey: currentMessage.groupingKey, threadId: currentMessage.threadId, timestamp: currentMessage.timestamp, flags: StoreMessageFlags(currentMessage.flags), tags: currentMessage.tags, globalTags: currentMessage.globalTags, localTags: currentMessage.localTags, forwardInfo: currentMessage.forwardInfo.flatMap(StoreMessageForwardInfo.init), authorId: currentMessage.author?.id, text: currentMessage.text, attributes: attributes, media: currentMessage.media))
                 })
             }
         } else {
